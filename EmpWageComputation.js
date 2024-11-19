7dcf1e2e50635d62ce7da9dc897f61a3a2e5401e
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-11-19 17:04:30 +0530

    [Vidwath]Added:Implemented Switch Case

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -8,7 +8,7 @@ async function EmpWageComputation() {
         let attendanceStatus = await checkAttendance();
         console.log(attendanceStatus);
         let empType = EmpType();
-        const calculateWage = await DailyWageCalculator(empType);
+        const calculateWage = DailyWageCalculator(empType);
         console.log(`Daily Wage of Employee is Rs.${calculateWage}`);
     }
     catch(error){
@@ -34,14 +34,36 @@ const PartTimeHour = 4;
 
 const DailyWageCalculator = (type) => {
 
-           const WorkHour = type ? FullDayHour:PartTimeHour;    
-           const Dailywage = WorkHour*WagePerHour;
-           return Dailywage;
+        let Dailywage;
+           switch (type) {
+                case 'fullTime':
+                    Dailywage = FullDayHour*WagePerHour;
+                    break;
+                case 'partTime':
+                    Dailywage = PartTimeHour*WagePerHour;
+                    break;
+                default:
+                    reject('Invalid employee type');
+            }
+           
+    return Dailywage;
 }
 
 const EmpType = ()=>{
-    const type = Math.random() < 0.5 ? "PartTime" : "FullTime"
+    const randomValue = Math.random()
+  //console.log(randomValue);
+    let type ;
+    switch (true) {
+        case randomValue < 0.5:
+            type = 'fullTime';
+            break;
+        case randomValue >= 0.5:
+            type = 'partTime';
+            break;
+        default:
+            type = 'unknown';
+    }
     console.log(`Employee Type is: ${type}`);
-    return type === "FullTime"
+    return type
 }
 EmpWageComputation();     
