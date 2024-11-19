ea6b86dd8bf5dd5e866f3222756eb7076bb1bba9
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-11-19 16:24:09 +0530

    [Vidwath]Added:Daily Wage

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -3,15 +3,41 @@ console.log("Welcome to Employee Wage Computation Program");
 }
 welcome()
 
-function checkAttendance(){
-
-    const status = Math.floor(Math.random() * 2)
-    console.log(status);
-    
-    if(status === 1)
-        console.log("Present");
-    else
-        console.log("Absent");
-} 
-checkAttendance();      
+async function EmpWageComputation() {
+    try{
+        let attendanceStatus = await checkAttendance();
+        const calculateWage = await DailyWageCalculator(attendanceStatus);
+        console.log(`Daily Wage of Employee is Rs.${calculateWage}`)
+    }
+    catch(error){
+        console.log(error);
         
+    }
+}
+
+const checkAttendance = () => {
+    return new Promise((resolve,reject) =>{
+        const status = Math.floor(Math.random() * 2)
+    
+        if(status === 1)
+            resolve("Present");
+        else
+            reject("Absent");
+    })   
+} 
+
+const FullDayHour = 8;
+const WagePerHour = 20;
+
+const DailyWageCalculator = (status) => {
+    return new Promise((resolve,reject) => {
+        if(status === "Present"){
+            const Dailywage = FullDayHour*WagePerHour;
+            resolve(Dailywage)
+        }
+        else{
+            reject("absent = nowork = nosalary")
+        }
+    })
+}
+EmpWageComputation();     
