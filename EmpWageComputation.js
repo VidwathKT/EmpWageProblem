e585c13421c11e80a438418cae95671c1e30cb9c
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-11-19 15:31:53 +0530

    [Vidwath]Added:Check Attendance

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -2,3 +2,16 @@ function welcome(){
 console.log("Welcome to Employee Wage Computation Program");
 }
 welcome()
+
+function checkAttendance(){
+
+    const status = Math.floor(Math.random() * 2)
+    console.log(status);
+    
+    if(status === 1)
+        console.log("Present");
+    else
+        console.log("Absent");
+} 
+checkAttendance();      
+        
